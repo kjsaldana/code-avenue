3c226b5aa9ac9fefaf8c670958c59df5ba6ecf0d
Kevin J <kjoseph1910@hotmail.com>
2026-07-06 16:08:07 +0200

    feat: add content model

---
+++ apps/courses/models/content.py
@@ -0,0 +1,11 @@
+from .module import Module
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name="contents", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
